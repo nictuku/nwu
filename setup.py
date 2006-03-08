51d6d901c13cd25805e4625e6141a82ddc5fd1f2
Mario Meyer <mariomeyer@ubuntu.com>
2006-03-08 08:02:58 +0000

    * Fix missing /etc/nwu/agent.conf on package install   by having a sample file in the package (Closes #36)

--- setup.py
+++ setup.py
@@ -12,4 +12,5 @@ setup(name='Nwu',
        'nwu-server/nwu', 'nwu-server/nwu-server'],
       package_dir = {'nwu_agent': 'nwu-agent/nwu_agent', 'nwu_server': 'nwu-server/nwu_server'},
       packages=['nwu_agent', 'nwu_server', 'nwu_server.db'],
+      data_files=[('/etc/nwu', ['nwu-agent/sample_conf/agent.conf'])]
      )
