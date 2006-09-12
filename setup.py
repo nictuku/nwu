fcc0d94d403bf49a734e436a7242845a6730b4f7
Yves Junqueira <yves.junqueira@gmail.com>
2006-09-12 05:56:00 +0000

    * Missing commit in the init procedure in the server
    * Fixing bad behaviour in the agent

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 from distutils.core import setup
 setup(name='Nwu',
-      version='0.1.1',
+      version='0.1.5',
       description='Network-wide Updates',
       author='Yves Junqueira',
       author_email='yves@cetico.org',
       maintainer='Yves Junqueira',
       maintainer_email='yves@cetico.org',
-      url='https://dev.ubuntubrasil.org/trac/nwu/',
+      url='http://cetico.org/nwu',
       scripts=['nwu-agent/nwu-agent', 'nwu-agent/nwu-maint', 
        'nwu-server/nwu', 'nwu-server/nwu-server'],
       package_dir = {'nwu_agent': 'nwu-agent/nwu_agent', 
