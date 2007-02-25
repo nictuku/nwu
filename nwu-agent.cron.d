c342e86b70d086fa86c1c5e8b8a2e893578cc1a7
Yves Junqueira <yves.junqueira@gmail.com>
2007-02-25 08:06:27 +0000

    nicer cron lines

--- nwu-agent.cron.d
+++ nwu-agent.cron.d
@@ -4,5 +4,5 @@
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 MAILTO=root
 
-0,10,20,30,40,50  *   * * *   nwuagent    /usr/bin/nwu-agent > /dev/null 2>&1
-5,15,25,35,45,55  *   * * *  root    /usr/sbin/nwu-maint > /dev/null 2>&1
+10-50/10  *   * * *   nwuagent    /usr/bin/nwu-agent > /dev/null 2>&1
+5-55/10  *   * * *  root    /usr/sbin/nwu-maint > /dev/null 2>&1
